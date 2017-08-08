6c646f1b47e49ac6c81327c9551740a7bd5cc4b6
Tshepang Lekhonkhobe <tshepang@gmail.com>
2017-08-08 02:07:08 +0200

    add code

---
+++ README.md
@@ -0,0 +1,21 @@
+# weeks-from-now
+
+[![Linux build status](https://travis-ci.org/tshepang/weeks-from-now.svg?branch=master)](https://travis-ci.org/tshepang/weeks-from-now)
+
+You may want to know date of 6 weeks from now:
+
+    $ weeks-from-now 6
+    2017-09-19
+
+You may even want to know date of 3 weeks ago:
+
+    $ weeks-from-now 3 --past
+    2017-07-18
+
+Following is the most easy way to install the tool
+(assuming you have the [Rust toolchain installed][install]):
+
+    cargo install weeks-from-now
+
+
+[install]: https://www.rust-lang.org/en-US/install.html
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+extern crate chrono;
+extern crate structopt;
+#[macro_use] extern crate structopt_derive;
+
+use chrono::{Duration, Local};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "weeks-from-now", about = "Date of x weeks from now")]
+struct Opt {
+    #[structopt(help = "Number of weeks")]
+    count: i64,
+    #[structopt(long = "past", help = "Count backwards")]
+    past: bool,
+}
+
+fn main() {
+    let today = Local::today().naive_local();
+    let opt = Opt::from_args();
+    let date = if opt.past {
+        today.checked_sub_signed(Duration::weeks(opt.count))
+    } else {
+        today.checked_add_signed(Duration::weeks(opt.count))
+    };
+    match date {
+        Some(date) => println!("{}", date),
+        None => eprintln!("Date not representable... overflow!"),
+    };
+}
